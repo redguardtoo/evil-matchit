9158dc87cc7a2f23a5c3259d82ef369851544d78
Chen Bin <chenbin.sh@gmail.com>
2015-02-04 21:43:29 +1100

    case sensitive code is cleaned because Emacs will handle it

--- evil-matchit.el
+++ evil-matchit.el
@@ -31,6 +31,7 @@
 ;;
 ;; This program emulates matchit.vim by Benji Fisher.
 ;; It allows you use % to match items.
+;; See https://github.com/redguardtoo/evil-matchit/ for help
 ;;
 ;; This program requires EVIL (http://gitorious.org/evil)
 ;;
