6346825fd89ee115fab974746fdba338adee856c
Chen Bin <chenbin.sh@gmail.com>
2016-04-13 23:25:03 +1000

    less picky about latex tags

--- evil-matchit.el
+++ evil-matchit.el
@@ -4,7 +4,7 @@
 
 ;; Author: Chen Bin <chenbin.sh@gmail.com>
 ;; URL: http://github.com/redguardtoo/evil-matchit
-;; Version: 2.1.2
+;; Version: 2.1.3
 ;; Keywords: matchit vim evil
 ;; Package-Requires: ((evil "1.0.7"))
 ;;
@@ -320,7 +320,7 @@ If font-face-under-cursor is NOT nil, the quoted string is being processed"
   (autoload 'evilmi-sh-jump "evil-matchit-sh" nil)
   (plist-put evilmi-plugins 'sh-mode '((evilmi-sh-get-tag evilmi-sh-jump)))
 
-  ;; Lua/Ruby ... any normal script languages
+  ;; Lua or any fine script
   (autoload 'evilmi-script-get-tag "evil-matchit-script" nil)
   (autoload 'evilmi-script-jump "evil-matchit-script" nil)
   (mapc (lambda (mode)
@@ -328,6 +328,7 @@ If font-face-under-cursor is NOT nil, the quoted string is being processed"
                                            (evilmi-script-get-tag evilmi-script-jump))))
         '(lua-mode vimrc-mode))
 
+  ;; Ruby
   (autoload 'evilmi-ruby-get-tag "evil-matchit-ruby" nil)
   (autoload 'evilmi-ruby-jump "evil-matchit-ruby" nil)
   ;; @see https://github.com/syl20bnr/spacemacs/issues/2093
@@ -436,7 +437,7 @@ If font-face-under-cursor is NOT nil, the quoted string is being processed"
    ))
 
 ;;;###autoload
-(defun evilmi-version() (interactive) (message "2.1.2"))
+(defun evilmi-version() (interactive) (message "2.1.3"))
 
 ;;;###autoload
 (define-minor-mode evil-matchit-mode
