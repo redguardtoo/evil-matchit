2b9b68770a384672e1fdc02326036a05d3cbaf01
Chen Bin <chenbin.sh@gmail.com>
2020-11-29 23:41:53 +1100

    fixed: cannot jump back from the line end in visual mode

--- evil-matchit-simple.el
+++ evil-matchit-simple.el
@@ -86,11 +86,31 @@ If it's t, use simple jump.")
         ;; sorry we could not jump between ends of string in python-mode
         (memq ch evilmi-quote-chars)))))
 
+(defun evilmi-simple-following-char ()
+  "Get the character at point or find matching tag start point nearby."
+  (let* ((b (line-beginning-position))
+         (whitespaces '(9 10 13 32))
+         new-pos)
+
+    (save-excursion
+      (let* (ch found)
+        (while (and (setq ch (following-char))
+                    (> (point) b)
+                    (not (setq found (or (memq ch evilmi-backward-chars)
+                                         (memq ch evilmi-forward-chars))))
+                    (memq ch whitespaces))
+          (goto-char (1- (point))))
+        (if found (setq new-pos (point)))))
+
+    (if new-pos (goto-char new-pos))
+
+    (following-char)))
+
 ;;;###autoload
 (defun evilmi-simple-get-tag ()
   "Get current tag in simple language."
   (let* (forward-line-num
-         (ch (following-char))
+         (ch (evilmi-simple-following-char))
          rlt)
 
     (cond
